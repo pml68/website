f5615a5b00aa2310e162488dd84303de074f6876
pml68 <tutorialmester@gmail.com>
2024-01-16 18:25:21 +0100

    feat: remove test square, add waves for the top corners

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
   import '../app.scss'
+  import wave1 from '$lib/assets/wave1.svg'
+  import wave2 from '$lib/assets/wave2.svg'
 </script>
 
 <svelte:head>
   <title>pml68</title>
 </svelte:head>
 
+<img src={wave1} alt="Wave 1" class="wave wave1">
+<img src={wave2} alt="Wave 2" class="wave wave2">
 <div class="container">
-  <div class="ii">
-    <h1>Hey boy</h1>
-  </div>
 </div>

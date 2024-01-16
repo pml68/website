c36955aa450dd3fa8668e7c91a7822a3c087eb38
pml68 <tutorialmester@gmail.com>
2024-01-16 19:35:26 +0100

    feat: add profile content, socials etc.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,15 @@
   import '../app.scss'
   import wave1 from '$lib/assets/wave1.svg'
   import wave2 from '$lib/assets/wave2.svg'
+
+  import { socials } from '$lib/socials.json'
+
+  type Social = {
+    link: string,
+    icon: string
+  }
+
+  const socialsList: Social[] = socials
 </script>
 
 <svelte:head>
@@ -11,4 +20,15 @@
 <img src={wave1} alt="Wave 1" class="wave wave1">
 <img src={wave2} alt="Wave 2" class="wave wave2">
 <div class="container">
+  <div class="main">
+    <h1>pml68</h1>
+    <p>Just your average IT student</p>
+    <div class="socials">
+      {#each socialsList as {link, icon}}
+        <a href={link} target="_blank">
+          <div class={'social-icon fa-brands ' + icon}></div>
+        </a>
+      {/each}
+    </div>
+  </div>
 </div>

08b62b2de02c02c8a4bbc2da2bfd11fe89424c95
pml68 <contact@pml68.me>
2024-10-30 00:39:03 +0100

    chore: upgrade to Svelte 5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,16 @@
 
   import WaveLeft from '$lib/assets/wave1.svg?url'
   import WaveRight from '$lib/assets/wave2.svg?url'
+  interface Props {
+    children?: import('svelte').Snippet;
+  }
+
+  let { children }: Props = $props();
 </script>
 
 <img src={WaveLeft} alt="" class="wave wave1" />
 <img src={WaveRight} alt="" class="wave wave2" />
 
 <div class="container">
-  <slot/>
+  {@render children?.()}
 </div>
